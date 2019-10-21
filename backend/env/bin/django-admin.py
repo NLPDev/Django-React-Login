a22a722ce3d28bc74ab54feaeb8e2400bf7ecd0f
NLPDev <vasile123andronic@gmail.com>
2019-10-21 08:01:41 -0700

    initial commit

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/Untitled/Django/Django_React_Login/DjReact/backend/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ frontend/gui/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk';
+
+import reducer from './store/reducers/auth';
+
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhances(
+    applyMiddleware(thunk)
+));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
+registerServiceWorker();
